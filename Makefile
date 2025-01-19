bd27d438475e8439195d73e83357c2139a57bd53
Lawrence D. Kern <law@lawkern.com>
2025-01-18 22:19:06 -0500

    Implement Langton's Ant cellular automaton

--- Makefile
+++ Makefile
@@ -6,9 +6,11 @@ compile:
 	$(CC) $(CFLAGS) -c ./src/platform_sdl3.c     -o ./build/sdl3.o
 	$(CC) $(CFLAGS) -c ./src/platform_terminal.c -o ./build/terminal.o
 
-	$(CC) $(CFLAGS) ./build/sdl3.o ./src/rule110.c   -o ./build/rule110   $(LDFLAGS)
-	$(CC) $(CFLAGS) ./build/sdl3.o ./src/wireworld.c -o ./build/wireworld $(LDFLAGS)
+	$(CC) $(CFLAGS) ./build/sdl3.o ./src/rule110.c      -o ./build/rule110      $(LDFLAGS)
+	$(CC) $(CFLAGS) ./build/sdl3.o ./src/wireworld.c    -o ./build/wireworld    $(LDFLAGS)
+	$(CC) $(CFLAGS) ./build/sdl3.o ./src/langtons_ant.c -o ./build/langtons_ant $(LDFLAGS)
 
 run:
 #	./build/rule110
-	./build/wireworld
+#	./build/wireworld
+	./build/langtons_ant
---
+++ src/langtons_ant.c
@@ -0,0 +1,84 @@
+/* (c) copyright 2025 Lawrence D. Kern ////////////////////////////////////// */
+
+#include "platform.h"
+#include <stdio.h>
+
+enum
+{
+   WHITE,
+   BLACK,
+   ANT_WHITE,
+   ANT_BLACK,
+   GRID_STATE_COUNT,
+};
+
+typedef enum {
+   NORTH, EAST, SOUTH, WEST,
+} ant_direction;
+
+static u32 colors[GRID_STATE_COUNT] = {0xFFFFFFFF, 0x000000FF, 0xFF0000FF, 0xFF0000FF};
+
+#define GRID_WIDTH 120
+#define GRID_HEIGHT 100
+
+static u8 grid[GRID_HEIGHT][GRID_WIDTH];
+
+static ant_direction cw[] = {
+   [NORTH] = EAST,
+   [EAST] = SOUTH,
+   [SOUTH] = WEST,
+   [WEST] = NORTH,
+};
+
+static ant_direction ccw[] = {
+   [NORTH] = WEST,
+   [EAST] = NORTH,
+   [SOUTH] = EAST,
+   [WEST] = SOUTH,
+};
+
+int main(void)
+{
+   int ant_x = GRID_WIDTH / 3;
+   int ant_y = GRID_HEIGHT / 3;
+
+   grid[ant_y][ant_x] = ANT_WHITE;
+   ant_direction direction = NORTH;
+
+   platform_initialize("Langton's Ant Cellular Automata", GRID_WIDTH, GRID_HEIGHT);
+
+   while(1)
+   {
+      if(!platform_frame_begin())
+      {
+         break;
+      }
+
+      platform_render((u8 *)grid, colors);
+
+      grid[ant_y][ant_x] -= 2;
+      if(grid[ant_y][ant_x] == WHITE)
+      {
+         direction = cw[direction];
+         grid[ant_y][ant_x] = BLACK;
+      }
+      else // BLACK
+      {
+         direction = ccw[direction];
+         grid[ant_y][ant_x] = WHITE;
+      }
+
+      switch(direction)
+      {
+         case NORTH: { ant_y--; if(ant_y < 0)            ant_y += GRID_HEIGHT; } break;
+         case SOUTH: { ant_y++; if(ant_y >= GRID_HEIGHT) ant_y -= GRID_HEIGHT; } break;
+         case EAST:  { ant_x++; if(ant_x >= GRID_WIDTH)  ant_x -= GRID_WIDTH; } break;
+         case WEST:  { ant_x--; if(ant_x < 0)            ant_x += GRID_WIDTH; } break;
+      }
+      grid[ant_y][ant_x] += 2;
+
+      platform_frame_end();
+   }
+
+   return(0);
+}
